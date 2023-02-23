cf22c89df42fe88477b47f0fad9ca158d61ea94e
fareezatariq <fareezatariq@gmail.com>
2023-02-24 03:39:05 +0500

    added keyEventHandler to turnOff keyboard on enter

--- app/src/main/java/com/hfad/tipout/MainActivity.kt
+++ app/src/main/java/com/hfad/tipout/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.hfad.tipout
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.hfad.tipout.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -12,7 +16,8 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateTip.setOnClickListener{calculateTip()}
-    }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+        }
 
     private fun calculateTip() {
         val stringInTextField= binding.costOfServiceEditText.text.toString()
@@ -32,5 +37,15 @@ class MainActivity : AppCompatActivity() {
         }
         val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text=getString(R.string.tip_amount, formattedTip)
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
